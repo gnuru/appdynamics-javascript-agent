0409c559e637aa59386da4ab9e0e612dca7c15cf
John Reidy <john.reidy@qantasassure.com>
2018-06-26 11:14:08 +1000

    add embedJS script and JS url

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ exports.loadAgentFromCDN = function(key, spa2=false) {
       config.spa = { "spa2": spa2 };
   })(window['adrum-config'] || (window['adrum-config'] = {}));
   var adRum = require('./adrum.js');
+};
+
+/* 
+ * Return string for local embedding of browser agent
+ * eg:
+ * const appDynamicsLoadJS = appdynamics.loadAgentClientStr('AD-AAB-AAJ-XVV', true);
+ * {!isLocalDev && <script defer dangerouslySetInnerHTML={{ __html: appDynamicsLoadJS }} />}
+ * */
+exports.loadAgentClientStr = function(key, spa2=false) {
+return `var key='`+key+`'; console.log('appdAgent loading');  window['adrum-start-time'] = new Date().getTime(); (function(config){ config.appKey = key; config.adrumExtUrlHttp = 'http://cdn.appdynamics.com'; config.adrumExtUrlHttps = 'https://cdn.appdynamics.com'; config.beaconUrlHttp = 'http://col.eum-appdynamics.com'; config.beaconUrlHttps = 'https://col.eum-appdynamics.com'; config.xd = {enable : false}; config.spa = { 'spa2': `+spa2+` }; })(window['adrum-config'] || (window['adrum-config'] = {})); `;
+};
+
+/*
+ * Return url for main javascript code.
+ * will extend to support locally hosted code (e.g. adrum.js)
+ */
+exports.mainJSUrl = function(useCDN=true) {
+  return document.localhost.protocol + "//cdn.appdynamics.com/adrum/adrum-latest.js";
+}
+
+/*
+ * Include using require, main body of Javascript from this package
+ */
+exports.loadMainJS = function() {
+var adRum = require('./adrum.js');
 }
